1d57783cfbd5e791a20da78347c4f1b6bde05898
davywiz <davidhenry889@gmail.com>
2022-11-27 18:06:47 +0100

    Finished Cart Controller

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { GetUser } from 'src/common/decorators';
 import { CartService } from './cart.service';
+import { cartItemDto } from './common/dto';
 
 @Controller('cart')
 export class CartController {
-    constructor(private catService: CartService) { }
+    constructor(private cartService: CartService) { }
     //todo
     //add to cart 
     //remove from cart
@@ -12,18 +13,25 @@ export class CartController {
 
     @Get()
     getCart(@GetUser('id') userId: number) {
-        this.catService.getCart(userId);
+        return this.cartService.getCart(userId);
     }
 
     @Post()
-    addToCart() { }
-
-    @Post()
     @HttpCode(HttpStatus.OK)
-    removeProductQuantity() { }
+    addToCart(@Body() cartDto: cartItemDto, @GetUser('id') userId: number) {
+        return this.cartService.addToCart(cartDto, userId);
+    }
 
-    @Post()
+    @Post('reduce-product-quantity')
     @HttpCode(HttpStatus.OK)
-    deleteCart() { }
+    removeProductQuantity(@Body() cartDto: cartItemDto, @GetUser('id') userId: number) {
+        return this.cartService.removeProductQuantity(cartDto, userId);
+    }
+
+    @Post('remove-from-cart')
+    @HttpCode(HttpStatus.OK)
+    removeFromCart(@Body() cartDto: cartItemDto, @GetUser('id') userId: number) { 
+        return this.cartService.removeFromCart(cartDto, userId);
+    }
 
 }
--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -1,20 +1,227 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Cart, CartItem, Product, User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { cartItemDto } from './common/dto';
 
 @Injectable()
 export class CartService {
-
-
-    getCart(userId : number) {
+    constructor(private prismaService: PrismaService) {
 
     }
 
 
-    addToCart() { }
+    async getCart(userId: number) {
+        const user = await this.prismaService.user.findUnique({
+            where: {
+                id: userId,
+            },
+            include: {
+                cart: {
+                    include: {
+                        CartItem: {
+                            include: {
+                                product: true
+                            }
+                        }
+
+                    }
+                }
+            }
+        });
+
+        return user.cart.CartItem;
+    }
 
 
-    removeProductQuantity() { }
+    async addToCart(cartDto: cartItemDto, userId: number) {
+        let newQuantity = 1;
+        let addCart: User & {
+            cart: Cart & {
+                CartItem: (CartItem & {
+                    product: Product;
+                })[];
+            };
+        }
+
+        const user = await this.prismaService.user.findUnique({
+            where: {
+                id: userId,
+            },
+            include: {
+                cart: true
+            }
+        });
+        const product = await this.prismaService.product.findUnique({
+            where: {
+                id: cartDto.productId,
+            },
+
+        });
+        if (!product) {
+            throw new NotFoundException('Can\'t find product to add to cart')
+        }
+
+        if (user.cart === null) {
+            addCart = await this.addToCartIfNotExist(userId, newQuantity, cartDto);
+        } else {
+            addCart = await this.addToCartIfCartExists(userId, newQuantity, cartDto, user.cart.id)
+        }
+
+        return addCart.cart.CartItem;
+    }
 
 
-    deleteCart() { }
+    async removeProductQuantity(cartDto: cartItemDto, userId: number) {
+        const user = await this.checkUserAndCart(userId, cartDto);
 
+        const updateCartItem = await this.prismaService.cartItem.update({
+            where: {
+                cartId_productId: { cartId: user.cart.id, productId: cartDto.productId }
+            },
+            data: {
+                quantity: {
+                    decrement: 1
+                }
+            }
+        });
+
+        if (updateCartItem.quantity === 0) {
+            await this.prismaService.cartItem.delete({
+                where: {
+                    cartId_productId: { cartId: user.cart.id, productId: cartDto.productId }
+                },
+            })
+        }
+
+        return await this.getCart(userId);
+
+    }
+
+
+    async removeFromCart(cartDto: cartItemDto, userId: number) {
+
+        const user = await this.checkUserAndCart(userId, cartDto);
+
+        const ifExist = await this.prismaService.cartItem.findUnique({
+            where: {
+                cartId_productId: { cartId: user.cart.id, productId: cartDto.productId }
+            },
+        })
+
+        if (!ifExist) {
+            throw new NotFoundException();
+        }
+        
+        await this.prismaService.cartItem.delete({
+            where: {
+                cartId_productId: { cartId: user.cart.id, productId: cartDto.productId }
+            },
+        })
+
+
+        return await this.getCart(userId);
+
+    }
+
+
+    //helper functions
+    async checkUserAndCart(userId: number, cartDto: cartItemDto) {
+        const user = await this.prismaService.user.findUnique({
+            where: {
+                id: userId,
+            },
+            include: {
+                cart: true
+            }
+        });
+        if (user.cart === null) {
+            throw new NotFoundException('No cart found')
+        }
+        const product = await this.prismaService.product.findUnique({
+            where: {
+                id: cartDto.productId,
+            },
+
+        });
+        if (!product) {
+            throw new NotFoundException('Can\'t find product to add to cart')
+        }
+
+        return user;
+    }
+
+    addToCartIfNotExist(userId: number, newQuantity: number, cartDto: cartItemDto) {
+        return this.prismaService.user.update({
+            where: {
+                id: userId,
+            },
+            data: {
+                cart: {
+                    create: {
+                        CartItem: {
+                            create: {
+                                quantity: newQuantity,
+                                productId: cartDto.productId
+                            }
+                        }
+                    },
+
+                }
+            },
+            include: {
+                cart: {
+                    include: {
+                        CartItem: {
+                            include: {
+                                product: true
+                            }
+                        }
+
+                    }
+                }
+            }
+        });
+    }
+
+    addToCartIfCartExists(userId: number, newQuantity: number, cartDto: cartItemDto, cartId: number) {
+        return this.prismaService.user.update({
+            where: {
+                id: userId,
+            },
+            data: {
+                cart: {
+                    update: {
+                        CartItem: {
+                            upsert: {
+                                where: {
+                                    cartId_productId: { cartId, productId: cartDto.productId }
+                                },
+                                update: {
+                                    quantity: {
+                                        increment: 1
+                                    },
+                                },
+                                create: {
+                                    quantity: newQuantity,
+                                    productId: cartDto.productId
+                                },
+                            }
+                        }
+                    },
+                }
+            },
+            include: {
+                cart: {
+                    include: {
+                        CartItem: {
+                            include: {
+                                product: true
+                            }
+                        }
+
+                    }
+                }
+            }
+        });
+    }
 }
